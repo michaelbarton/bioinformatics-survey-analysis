9257b9af8b63f915695aa4cffc67e3fd46d84f11
Michael Barton <mail@michaelbarton.me.uk>
2012-03-20 13:47:59 -0400

    Create crossbar plot for satisfaction

--- plot/satisfaction/with_position.r
+++ plot/satisfaction/with_position.r
@@ -5,18 +5,30 @@ library(ggplot2)
 source('lib/generate_plot_file.r')
 source('lib/survey.r')
 
-p <- ggplot(
-  subset(survey.2012(), ! is.na(position)),
-  aes( y = satisfaction, x = filtered.region))
+stftn <- ddply(survey.2012(), .(agg.position, filtered.region), function(df){
+  with(df,{
+    data.frame(
+      median = if(length(satisfaction) > 10) median(satisfaction) else NA,
+      mad    = if(length(satisfaction) > 10) mad(satisfaction)    else NA
+    )
+  })
+})
 
-p <- p + stat_boxplot()
-p <- p + facet_grid(position ~ .)
+p <- ggplot(
+  data.frame(lapply(na.omit(subset(stftn,filtered.region != "Other")), function(x) x[drop=TRUE])),
+  aes(y    = median,
+      ymin = median - mad,
+      ymax = median + mad,
+      x    = agg.position,
+      col  = filtered.region))
+
+p <- p + geom_crossbar(position = position_dodge(width=0.90) ,lwd=1, width = 0.6)
 p <- p + coord_flip()
 p <- p + theme_bw()
 p <- p + scale_x_discrete("")
+p <- p + scale_color_brewer("Region",palette="Set2")
 p <- p + scale_y_continuous("Feeling of satisfaction (0-9)")
 p <- p + opts(legend.position = "top")
 p <- p + opts(axis.text.x=theme_text(angle=-90, hjust=0))
-
 
 generate_plot_file(p,'satisfaction_and_position.png',height=960)
