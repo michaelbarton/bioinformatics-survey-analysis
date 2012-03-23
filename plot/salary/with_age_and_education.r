2838f4c0fad7d10fa22ced6084840444be2a408d
Michael Barton <mail@michaelbarton.me.uk>
2012-03-23 12:05:36 -0400

    Add rlm smooth to salary plot

--- plot/salary/with_age_and_education.r
+++ plot/salary/with_age_and_education.r
@@ -2,6 +2,7 @@
 
 library(ggplot2)
 
+
 source('lib/survey.r')
 source('lib/generate_plot_file.r')
 
@@ -12,11 +13,12 @@ d <- na.omit(
              salary = if(length(salary) < 10) NA else salary,
              age    = age )})}))
 
-p <- ggplot(d,aes(x=age, y=salary, col=factor(agg.sector)))
+p <- ggplot(d,aes(x=age, y=floor(salary), col=factor(agg.sector)))
 p <- p + geom_point()
 p <- p + facet_grid(region ~ .)
 p <- p + scale_color_brewer("Region",palette="Set2")
 p <- p + theme_bw()
+p <- p + stat_smooth(method=MASS::rlm)
 p <- p + opts(legend.position = "top")
 
 generate_plot_file(p,'salary_with_age_and_region.png',height=1000)
