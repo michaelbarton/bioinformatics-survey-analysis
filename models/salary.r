c95cfcf92cb37e4840d874ddd38116a0121ccaed
Michael Barton <mail@michaelbarton.me.uk>
2012-03-25 15:40:52 -0400

    Build simplified model for salary

--- models/salary.r
+++ models/salary.r
@@ -45,8 +45,66 @@ scaled.data <- within(na.omit(academics),{
   agg.region   <- factor(agg.region)
   agg.position <- factor(agg.position)
   gender       <- factor(gender)
-
 })
 
-summary(scaled.data)
+full.model <- lm(salary ~
 
+              # Identity variables
+              agg.region    +
+              gender        +
+              age           +
+              agg.position  +
+              hours         +
+              publications  +
+              first         +
+              corresponding +
+              grants        +
+              size          +
+
+              # Quadratic terms
+              I(age^2)           +
+              I(hours^2)         +
+              I(publications^2)  +
+              I(first^2)         +
+              I(corresponding^2) +
+              I(grants^2)        +
+              I(size^2)          +
+
+              # Interaction of region with variables
+              agg.region:gender        +
+              agg.region:age           +
+              agg.region:agg.position  +
+              agg.region:hours         +
+              agg.region:publications  +
+              agg.region:first         +
+              agg.region:corresponding +
+              agg.region:grants        +
+              agg.region:size          +
+
+              # Interaction of region with quadratic variables
+              agg.region:I(age^2)           +
+              agg.region:I(hours^2)         +
+              agg.region:I(publications^2)  +
+              agg.region:I(first^2)         +
+              agg.region:I(corresponding^2) +
+              agg.region:I(grants^2)        +
+              agg.region:I(size^2),
+
+              scaled.data)
+
+
+model.1 <- step(full.model)
+summary(model.1)
+
+model.2 <- update(model.1, ~ .
+                  - size
+                  - I(publications^2)
+                  - I(size^2)
+                  - I(grants^2)
+                  - agg.region:I(grants^2)
+                  - agg.region:I(publications^2)
+                  - agg.region:size)
+
+summary(model.2)
+
+anova(model.1,model.2)
