ca84d94e77eccf4b76282230e98945122d8251b6
Michael Barton <mail@michaelbarton.me.uk>
2012-03-26 11:44:28 -0400

    Update script to name outliers in data

--- models/salary/plot_variables.r
+++ models/salary/plot_variables.r
@@ -7,7 +7,10 @@ d <- salary.regression.data()
 plots <- sapply(names(d),function(name){
    var = d[,name]
    if(typeof(var) == "double"){
+     outliers <- head(order(var^2,decreasing=TRUE))
+
      png(paste("salary_model_",name,".png",sep=""))
      plot(var)
+     text(outliers,var[outliers],outliers,pos=4)
    }
 })
