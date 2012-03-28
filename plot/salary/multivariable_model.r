d949998c800e116a828e2057e510b6e79a8ac074
Michael Barton <mail@michaelbarton.me.uk>
2012-03-28 18:05:13 -0400

    Order and rename factor levels in regression plot

--- plot/salary/multivariable_model.r
+++ plot/salary/multivariable_model.r
@@ -15,6 +15,7 @@ coefs <- within(coefs,{
   X[X == "agg.regionEurope"] <- "(Intercept)"
 })
 
+
 marginals <- ddply(coefs,.(X,region),function(x){
   with(x,{
     data.frame(
@@ -26,25 +27,59 @@ marginals <- ddply(coefs,.(X,region),function(x){
 })
 
 
-# Specify constants across regions
 variable.freq  <- count(marginals$X)
 constant.terms <- marginals$X %in% variable.freq$x[variable.freq$freq == 1]
-marginals$region[constant.terms] <- ""
+marginals <- within(marginals,{
+  region[constant.terms] <- "None"
+  region <- factor(region,c("None","Northern America","Europe"))
 
-marginals$term.type                 <- "Region interaction"
-marginals$term.type[constant.terms] <- "Constant"
 
+  X <- sapply(X,function(x){
+    switch(x,
+      size          = "Largest Grant Size ($)",
+      publications  = "Total Publications",
+      hours         = "Average Weekly Hours Worked",
+      grants        = "Total Grants",
+      genderMale    = "Male Gender",
+      first         = "First Author Publications",
+      corresponding = "Corresponding Author Publications",
+      conferences   = "Average Conference Attendence",
+      age           = "Age",
+
+      "agg.positionPI / Management"       = "PI / Management",
+      "agg.positionPost Doctoral (Staff)" = "Post Doctoral (Staff)",
+      x
+    )
+  })
+  X <- factor(X,rev(c(
+      "Male Gender",
+      "Average Conference Attendence",
+      "Average Weekly Hours Worked",
+      "(Intercept)",
+      "Total Grants",
+      "First Author Publications",
+      "Corresponding Author Publications",
+      "Largest Grant Size ($)",
+      "Total Publications",
+      "Age",
+      "PI / Management",
+      "Post Doctoral (Staff)"
+      )))
+})
+
+# Specify constants across regions
 
 p <- ggplot(marginals,
-            aes(x   = X,
+            aes(x   = factor(X),
                 y   = estimate,
                 col = region))
 
-p <- p + facet_wrap( ~ term.type, scales = "free",space="free",ncol=1)
 p <- p + geom_point()
 p <- p + coord_flip()
 p <- p + theme_bw()
-p <- p + scale_y_continuous("Estimated Regression Coefficient",
-                            limits=c(-75000,150000))
+p <- p + scale_color_hue("Interaction Term")
+p <- p + scale_y_continuous("Estimated Regression Coefficient")
+p <- p + scale_x_discrete("")
 p <- p + geom_abline(v=0,lty=2,col="grey")
+p <- p + opts(legend.position = "top")
 p
