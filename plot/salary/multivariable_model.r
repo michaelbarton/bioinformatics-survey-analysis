6d69cc0c2c6a8f4465df16cd17116b27a7d2a75b
Michael Barton <mail@michaelbarton.me.uk>
2012-03-29 14:22:12 -0400

    Estimate standard errors

--- plot/salary/multivariable_model.r
+++ plot/salary/multivariable_model.r
@@ -6,6 +6,7 @@ library(ggplot2)
 source('lib/generate_plot_file.r')
 
 coefs <- read.csv("./model_salary_estimates.csv")
+covs  <- melt(dget("./model_salary_covariance.R"))
 
 coefs <- within(coefs,{
   region <- rep("Northern America",length(X))
@@ -19,9 +20,19 @@ coefs <- within(coefs,{
 marginals <- ddply(coefs,.(X,region),function(x){
   with(x,{
     data.frame(
+
       estimate = Value + if(region == "Europe"){
           coefs$Value[coefs$region == "Europe" & coefs$X == X]
-        } else 0
+        } else { 0 },
+
+      error = if(region != "Europe"){
+        coefs$Std..Error[coefs$region != "Europe" & coefs$X == X]
+      } else {
+        int = if(X == "(Intercept)") "agg.regionEurope" else paste("agg.regionEurope",X,sep=":")
+
+        sqrt(sum(coefs$Std..Error[coefs$X %in% X]) +
+          2 * covs$value[covs$X1 == X & covs$X2 == int])
+      }
     )
   })
 })
@@ -70,8 +81,10 @@ marginals <- within(marginals,{
 # Specify constants across regions
 
 p <- ggplot(marginals,
-            aes(x   = factor(X),
-                y   = estimate,
+            aes(x    = factor(X),
+                y    = estimate,
+                ymin = estimate - error,
+                ymax = estimate + error,
                 col = region))
 
 p <- p + geom_point()
@@ -82,4 +95,5 @@ p <- p + scale_y_continuous("Estimated Regression Coefficient")
 p <- p + scale_x_discrete("")
 p <- p + geom_abline(v=0,lty=2,col="grey")
 p <- p + opts(legend.position = "top")
-p
+
+generate_plot_file(p,'salary_model_effects.png',height=700)
